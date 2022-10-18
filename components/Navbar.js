11fc48ace55d897fb1fd7127bce1590dbf7ba67a
Jesse Songok <jessesongok@gmail.com>
2022-10-18 19:20:37 +0300

    Initial Commit

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return(
+        <div className="navbar bg-success text-success-content">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="form-control">
+                    <input type="text" placeholder="Search" className="input input-bordered"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
