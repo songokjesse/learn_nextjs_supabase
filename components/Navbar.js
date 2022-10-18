507595bc60983f0b3ca9c0c3171d5ff9c94fa02e
Jesse Songok <jessesongok@gmail.com>
2022-10-18 19:36:00 +0300

    Edited Titles

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
     return(
         <div className="navbar bg-success text-success-content">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">My School</a>
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
